d65b0bb414f052b68b78bfb927db4697056b09a4
steban <josueayo.ejat@gmail.com>
2018-11-05 12:01:53 -0500

    En la clase aplicación se creo el método main con 2 sout de información

---
+++ src/ec/edu/ister/modelo/Movie.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.modelo;
+
+import ec.edu.ister.modelo.interfaces.IVisualizable;
+import java.util.Date;
+
+/**
+ *
+ * @author Alvarez
+ */
+public class Movie extends Film implements IVisualizable {
+
+    private int id;
+    private int timeViewed;
+
+    public Movie(String title, String genre, String creator, int duration, short year) {
+        super(title, genre, creator, duration);
+        setYear(year);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    @Override
+    public String toString() {
+        return "\n :: MOVIE ::"
+                + "\n Title: " + getTitle()
+                + "\n Género: " + getGenre()
+                + "\n Year: " + getYear()
+                + "\n Creator: " + getCreator()
+                + "\n Duration: " + getDuration();
+    }
+
+    @Override
+    public Date startToSee(Date dateI) {
+        return dateI;
+    }
+
+    @Override
+    public void stopToSee(Date dateI, Date dateF) {
+
+        if (dateF.getTime() > dateI.getTime()) {
+            setTimeViewed((int) (dateF.getTime() - dateI.getTime()));
+        } else {
+            setTimeViewed(0);
+        }
+    }
+}
---
+++ src/ec/edu/ister/modelo/interfaces/IVisualizable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.modelo.interfaces;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Alvarez
+ */
+public interface IVisualizable {
+
+    public Date startToSee(Date dateI);
+
+    public void stopToSee(Date dateI, Date dateF);
+
+}
